e3e2cbed601b1c9f2a8561520f3c8e3f0b26a4d7
kento <kentozuka22@gmail.com>
2020-06-15 20:16:48 +0900

    all done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 // requesting when the page is loaded
 window.addEventListener('DOMContentLoaded', async () => {
+  // total number
+  const ttt = await axios.get('https://api.covid19api.com/world/total')
+  const confirmedHolder = document.getElementById('total-confirmed')
+  const deathsHolder = document.getElementById('total-deaths')
+  const recoveredHolder = document.getElementById('total-recovered')
+  confirmedHolder.innerText = numberWithComma(ttt.data.TotalConfirmed)
+  deathsHolder.innerText = numberWithComma(ttt.data.TotalDeaths)
+  recoveredHolder.innerText = numberWithComma(ttt.data.TotalRecovered)
   // creating country list
   const cntrs = await axios.get('https://api.covid19api.com/countries')
   const cs = document.getElementById('country-selection')
@@ -22,7 +30,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   const map = L.map('map').setView([51.505, -0.09], 13)
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 5,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -36,7 +44,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     data: {
       labels: [0, 0],
       datasets: [{
-        label: '# of Cases',
+        label: 'Please select a country',
         data: [0, 0]
       }]
     },
@@ -52,12 +60,17 @@ window.addEventListener('DOMContentLoaded', async () => {
   })
 
   const selector = document.getElementById('country-selection')
-  selector.addEventListener('change', () => selected(chart))
+  selector.addEventListener('change', () => selected(chart, map))
 })
 
-async function selected(target) {
+async function selected(target, map) {
   const name = document.getElementById('country-selection').value
   const api = await axios.get(`https://api.covid19api.com/country/${name}`)
+  // for leaflet
+  const lat = api.data ? api.data[0].Lat : '0'
+  const lon = api.data ? api.data[0].Lon : '0'
+  const pss = [lat, lon]
+
   const conf = api.data.map(x => x.Confirmed)
   const deat = api.data.map(x => x.Deaths)
   const reco = api.data.map(x => x.Recovered)
@@ -65,6 +78,15 @@ async function selected(target) {
     const dt = new Date(x.Date)
     return `${dt.getMonth() + 1}/${dt.getDate()}`
   })
+
+  const size = {
+    conf: (conf[conf.length - 1] / totals().conf) * 10000000,
+    deat: (deat[deat.length - 1] / totals().deat) * 10000000,
+    reco: (reco[reco.length - 1] / totals().reco) * 10000000
+  }
+  circles(map, size, pss)
+
+  // modifying chart
   const sets = [{
     label: 'Confirmed',
     data: conf,
@@ -82,6 +104,31 @@ async function selected(target) {
     backgroundColor: 'rgba(0, 0, 0, 0)'
   }]
   chart(target, sets, dates)
+  // manipulating leaflets
+  if (lat && lon) map.panTo(pss)
+}
+
+function circles(map, size, pss) {
+  const colors = {
+    border: {
+      conf: 'blue',
+      deat: 'red',
+      reco: 'green',
+    },
+    fill: {
+      conf: '',
+      deat: '',
+      reco: ''
+    }
+  }
+  for (const em in size) {
+    L.circle(pss, {
+      color: colors.border[em],
+      fillColor: colors.fill[em],
+      fillOpacity: 0.2,
+      radius: size[em],
+    }).addTo(map)
+  }
 }
 
 function chart(chart, datasets, labels) {
@@ -92,23 +139,27 @@ function chart(chart, datasets, labels) {
   chart.update()
 }
 
+// getting totals
+function totals() {
+  const confirmedHolder = document.getElementById('total-confirmed')
+  const deathsHolder = document.getElementById('total-deaths')
+  const recoveredHolder = document.getElementById('total-recovered')
+  return {
+    conf: number(confirmedHolder.innerText),
+    deat: number(deathsHolder.innerText),
+    reco: number(recoveredHolder.innerText)
+  }
+}
+
 // saving a few lines of code here
 function write(parent, node) {
   parent.appendChild(node)
 }
 
-// deleting elements from target item
-function remove(target) {
-  const t = document.querySelector(target)
-  if (t) t.parentNode.removeChild(t)
-}
-function reset(item) {
-  console.log(item)
+function numberWithComma(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
-// create element with class if specified
-function create(tag, text, css = null) {
-  const item = document.createElement(tag)
-  item.innerText = text
-  if (css) item.classList.add(css)
+function number(x) {
+  return parseInt(x.match(/\d+/g).join(''))
 }
